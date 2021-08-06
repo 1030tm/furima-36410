9ec362c9c493a11d508e7bb56853c907788f817c
1030tm <moninoki0139@gmail.com>
2021-08-06 14:38:04 +0900

    本人情報の正規表現の設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_kana, presence: true
-  validates :last_kana, presence: true
+  with_options presence: true, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name 
+  end
+  with_options presence: true, format: {with: /\A[ァ-ヶ一-]+\z/, message: '全角文字(カタカナ)を使用してください' } do
+    validates :first_kana
+    validates :last_kana
+  end
   validates :birthday, presence: true
 end
