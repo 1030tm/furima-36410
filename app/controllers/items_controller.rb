033cc97b0bfa7eb9281a7e5b15b6f9d4fc6a6ad0
1030tm <moninoki0139@gmail.com>
2021-08-11 22:28:45 +0900

    商品出品の設定１
    item_name
    explanation

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,17 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :item_name, :explanation, :category_id, :condition_id, :postage_id, :consignor_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
 end
