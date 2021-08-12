71b27f440635f46c089139be5d5e73720e7c2580
1030tm <moninoki0139@gmail.com>
2021-08-12 12:05:04 +0900

    商品の状態の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,16 +5,21 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :item_name
     validates :explanation
-    validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-    validates :condition_id
-    validates :postage_id
-    validates :consignor_id
-    validates :shipping_day_id
+
+    with_options numericality: { other_than: 1 , message: "can't be blank"} do
+      validates :category_id
+      validates :condition_id
+      validates :postage_id
+      validates :consignor_id
+      validates :shipping_day_id
+    end
+
     validates :price
     validates :user
   end
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :condition
 
 end
