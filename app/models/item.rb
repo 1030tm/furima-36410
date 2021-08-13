5d6bb11f4b6b1598cbe9c12c682da4ea14a8c2d5
1030tm <moninoki0139@gmail.com>
2021-08-13 23:10:54 +0900

    Itemモデルの単体テストコード

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,7 +14,8 @@ class Item < ApplicationRecord
       validates :shipping_day_id
     end
 
-    validates :price, inclusion: [ in: 300..9999999 ], format: {with: /\A[0-9]+\z/, message: '半角数字を使用してください'}
+    validates :price, inclusion: { in: 300..9999999 }
+    validates :price_before_type_cast, format: { with: /\A[0-9]+\z/, message: "半角数字を使用してください" }
     validates :user
   end
 
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name                 { 'ボックス' }
+    explanation               { '箱です' }
+    category_id               { 2 }
+    condition_id              { 2 }
+    postage_id                { 2 }
+    consignor_id              { 2 }
+    shipping_day_id           { 2 }
+    price                     { 1000 }
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/mask_tedukuri_check.png'), filename: 'test_image.png')
+    end
   end
 end
