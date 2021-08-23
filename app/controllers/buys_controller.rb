5d321942b71e89e65859d9aed629b0f489b7a41f
1030tm <moninoki0139@gmail.com>
2021-08-23 17:55:39 +0900

    購入画面へ遷移

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,10 +1,16 @@
 class BuysController < ApplicationController
   
   def index
-    
+    @item = Item.find(params[:item_id])
   end
 
   def create
     
   end
+
+  private
+  def sell_item_params
+    params.require(:form).permit(:user_id, :item_id, :postal_code, :consignor_id, :city, :address, :building, :tel, :buy_id).merge(user_id: current_user.id)
+  end
+
 end
