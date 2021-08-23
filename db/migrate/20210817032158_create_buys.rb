cc6539f934faa16a3c542817818067f6e29ebdec
1030tm <moninoki0139@gmail.com>
2021-08-23 17:54:09 +0900

    deliveryモデルの作成

--- db/migrate/20210817032158_create_buys.rb
+++ db/migrate/20210817032158_create_buys.rb
@@ -6,14 +6,5 @@ class CreateBuys < ActiveRecord::Migration[6.0]
       t.timestamps
     end
 
-    create_table :deliveries do |t|
-      t.string :postal_code, null: false
-      t.integer :consignor_id, null: false
-      t.string :city, null: false
-      t.string :address, null: false
-      t.string :building
-      t.string :tel, null: false
-      t.references :buy, null: false, foreign_key: true
-    end
   end
 end
