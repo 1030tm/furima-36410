d755d04aa61bac293c3f9fb71c8d36b2eafe627a
1030tm <moninoki0139@gmail.com>
2021-08-23 17:54:56 +0900

    アソシエーションの設定

--- app/models/buy.rb
+++ app/models/buy.rb
@@ -1,15 +1,6 @@
 class Buy < ApplicationRecord
+  belongs_to :user
+  belongs_to :item
+  has_one    :delivery
   
-  with_options presence: true do
-    validates :user
-    validates :item
-    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'を入力してください' }
-    validates :consignor_id
-    validates :city
-    validates :address
-    validates :tel, format: { with: /\A\d{10,11}\z/, message: 'を入力してください' }
-    validates :buy
-  end
-
-  validates :building
 end
